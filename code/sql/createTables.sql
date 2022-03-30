dc64c7ebd08653845a3ee9711833700d84361d96
47217 - Afonso Machado <A47217@alunos.isel.pt>
2022-03-30 23:07:56 +0100

    Update createTable. #9

--- code/sql/createTables.sql
+++ code/sql/createTables.sql
@@ -15,3 +15,69 @@ CREATE TABLE CLASSROOM (
     PRIMARY KEY(id),
     FOREIGN KEY(orgId) REFERENCES ORGANIZATION(id)
 );
+
+CREATE TABLE TEACHER (
+    number int unique,
+    name varchar(50),
+    PRIMARY KEY(number)
+);
+
+CREATE TABLE GROUP (
+    id int identity,
+    cId int,
+    nMembers int,
+    PRIMARY KEY(id),
+    FOREIGN KEY(cId) REFERENCES CLASSROOM(id)
+);
+
+CREATE TABLE NOTE (
+    gId int,
+    date timestamp,
+    description varchar(200),
+    PRIMARY KEY(gId,date),
+    FOREIGN KEY(gId) REFERENCES GROUP(id),
+);
+
+CREATE TABLE STUDENT (
+    number int unique,
+    name varchar(50),
+    cId int,
+    gId int,
+    PRIMARY KEY(number),
+    FOREIGN KEY(cId) REFERENCES CLASSROOM(id),
+    FOREIGN KEY(gId) REFERENCES GROUP(id)
+);
+
+CREATE TABLE ASSIGNMENT (
+    id int identity,
+    releaseDate timestamp,
+    dueDate timestamp,
+    cId int,
+    description varchar(200),
+    PRIMARY KEY (id),
+    FOREIGN KEY (cId) REFERENCES CLASSROOM(id)
+);
+
+CREATE TABLE REPO (
+    url varchar(50) unique,
+    name varchar(50),
+    gId int,
+    assId int,
+    PRIMARY KEY (url),
+    FOREIGN KEY (gId) REFERENCES GROUP(id),
+    FOREIGN KEY (assId) REFERENCES ASSIGNMENT(id)
+);
+
+CREATE TABLE DELIVERY (
+    id int identity,
+    assId int,
+    date timestamp,
+    PRIMARY KEY (id),
+    FOREIGN KEY (assId) REFERENCES ASSIGNMENT(id)
+);
+
+CREATE TABLE TAG (
+    name varchar(50),
+    date timestamp,
+    PRIMARY KEY (name)
+);
