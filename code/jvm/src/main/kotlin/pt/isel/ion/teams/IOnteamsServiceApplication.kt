fd5c507b89945616f185afaf0402005f3b57de58
Martim Francisco <a46912@alunos.isel.pt>
2022-04-01 00:33:37 +0100

    i-on teams service #5
    Initialization of spring project for the i-on teams service.

---
+++ code/jvm/src/main/kotlin/pt/isel/ion/teams/IOnteamsServiceApplication.kt
@@ -0,0 +1,11 @@
+package pt.isel.ion.teams
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IOnteamsServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<IOnteamsServiceApplication>(*args)
+}
---
+++ code/jvm/src/test/kotlin/pt/isel/ion/teams/IOnteamsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package pt.isel.ion.teams
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IOnteamsServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
