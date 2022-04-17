9ea61228a91ad3765ad40f9fe5a3ebca2b2643d7
47217 - Afonso Machado <A47217@alunos.isel.pt>
2022-04-17 11:46:02 +0100

    Service updates.

--- code/jvm/src/main/kotlin/pt/isel/ion/teams/assignments/Assignments.kt
+++ code/jvm/src/main/kotlin/pt/isel/ion/teams/assignments/Assignments.kt
@@ -13,12 +13,14 @@ data class AssignmentDbRead (
 )
 
 data class AssignmentDbWrite(
+    val releaseDate: Timestamp,
     val classId: Int,
     val description: String,
 )
 
 data class AssignmentDbUpdate(
     val id: Int,
+    val releaseDate: Timestamp?,
     val description: String?,
 )
 
@@ -28,35 +30,30 @@ data class AssignmentDbUpdate(
 
 data class AssignmentOutputModel(
     val id: Int,
-    val name: String,
+    val releaseDate: Timestamp,
+    val classId: Int,
     val description: String,
-    val schoolYear: String,
-    val state: String,
 )
 
 data class AssignmentInputModel(
-    val name: String,
+    val releaseDate: Timestamp,
+    val classId: Int,
     val description: String,
-    val maxGroups: Int,
-    val maxGroupMembers: Int,
-    val schoolYear: String,
 )
 
 data class AssignmentUpdateModel(
-    val name: String?,
+    val id: Int,
+    val releaseDate: Timestamp?,
     val description: String?,
-    val maxGroups: Int?,
-    val maxGroupMembers: Int?,
-    val state: String?,
 )
 
 /**
  * Functions to transition from external to internal, or vice-versa.
  */
 
-fun AssignmentInputModel.toDb(repoURI: String, orgId: Int) =
-    AssignmentDbWrite(this.name, this.description, this.maxGroups, this.maxGroupMembers, repoURI, this.schoolYear, orgId)
+fun AssignmentInputModel.toDb() =
+    AssignmentDbWrite(this.releaseDate,this.classId,this.description)
 fun AssignmentUpdateModel.toDb(id: Int) =
-    AssignmentDbUpdate(id, this.name, this.description, this.maxGroups, this.maxGroupMembers, this.state)
-fun AssignmentDbRead.toOutput() = AssignmentOutputModel(this.id, this.name, this.description, this.schoolYear, this.state)
+    AssignmentDbUpdate(id,this.releaseDate,this.description)
+fun AssignmentDbRead.toOutput() = AssignmentOutputModel(this.id,this.releaseDate,this.classId,this.description)
 
