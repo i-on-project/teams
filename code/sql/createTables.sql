dac46f55a8e0e00c8f7b247eb6ad82d17dd685fc
47217 - Afonso Machado <A47217@alunos.isel.pt>
2022-04-21 15:23:48 +0000

    Tables Teachers and Studente. Not null contraint added.

--- code/sql/createTables.sql
+++ code/sql/createTables.sql
@@ -9,13 +9,13 @@ CREATE TABLE ORGANIZATIONS
 CREATE TABLE CLASSROOMS
 (
     id                 serial,
-    name               varchar(50),
-    description        varchar(200),
-    maxGroups          int,
-    maxMembersPerGroup int,
-    linkRepo           varchar(50),
-    schoolYear         varchar, -- (e.g. 2021/22)
-    orgId              int,     --organization id
+    name               varchar(50)  NOT NULL,
+    description        varchar(200) NOT NULL,
+    maxGroups          int          NOT NULL,
+    maxMembersPerGroup int          NOT NULL,
+    linkRepo           varchar(50)  NOT NULL,
+    schoolYear         varchar      NOT NULL, -- (e.g. 2021/22)
+    orgId              int          NOT NULL, --organization id
     state              varchar(50) DEFAULT 'active',
     PRIMARY KEY (id),
     FOREIGN KEY (orgId) REFERENCES ORGANIZATIONS (id),
@@ -25,40 +25,37 @@ CREATE TABLE CLASSROOMS
 
 CREATE TABLE INVITE_LINKS
 (
-    link varchar,
-    cId  int, --classroom id
+    link varchar NOT NULL,
+    cId  int     NOT NULL, --classroom id
     PRIMARY KEY (link),
     FOREIGN KEY (cId) REFERENCES CLASSROOMS (id)
 );
 
 CREATE TABLE TEACHER
 (
-    number int unique,
-    name   varchar(50),
-    email  varchar,
-    office varchar(20),                                                                      --X.X.XX (e.g G.1.16)
+    number int unique  NOT NULL,
+    name   varchar(50) NOT NULL,
+    email  varchar     NOT NULL,
+    office varchar(20) NOT NULL,                                                             --X.X.XX (e.g G.1.16)
     cId    int,
     PRIMARY KEY (number),
     FOREIGN KEY (cId) REFERENCES CLASSROOMS (id),
     CONSTRAINT email_check CHECK (email ~* '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$') --TODO: not sure what it is (CHECK)
 );
 
-CREATE VIEW TEACHERS
-            (
-             number,
-             name,
-             email,
-             office,
-             cId)
-AS
-SELECT number, name, email, office, cId
-FROM TEACHER;
+CREATE TABLE TEACHERS
+(
+    number int unique NOT NULL,
+    cId    int        NOT NULL,
+    PRIMARY KEY (number),
+    FOREIGN KEY (cId) REFERENCES CLASSROOMS (id)
+);
 
 CREATE TABLE TEAMS
 (
     id    serial,
-    name  varchar(50),
-    cId   int, --class id
+    name  varchar(50) NOT NULL,
+    cId   int         NOT NULL, --class id
     state varchar(50) DEFAULT 'pending',
     PRIMARY KEY (id),
     FOREIGN KEY (cId) REFERENCES CLASSROOMS (id),
@@ -68,39 +65,40 @@ CREATE TABLE TEAMS
 CREATE TABLE NOTES
 (
     id          serial,
-    tId         int, --team id
-    date        timestamp,
-    description varchar(200),
+    tId         int          NOT NULL, --team id
+    date        timestamp    NOT NULL,
+    description varchar(200) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (tId) REFERENCES TEAMS (id)
 );
 
 CREATE TABLE STUDENT
 (
-    number int,
-    name   varchar(50),
-    tId    int, --team id
-    cId    int, --class id
+    number int         NOT NULL,
+    name   varchar(50) NOT NULL,
+    tId    int         NOT NULL, --team id
+    cId    int         NOT NULL, --class id
     PRIMARY KEY (number),
-    FOREIGN KEY (tId) REFERENCES TEAMS (id)
+    FOREIGN KEY (tId) REFERENCES TEAMS (id),
+    FOREIGN KEY (cId) REFERENCES CLASSROOMS (id)
 );
 
-CREATE VIEW STUDENTS
-            (
-             number,
-             name,
-             tId,
-             cId)
-AS
-SELECT number, name,tId, cId
-FROM STUDENT;
+CREATE TABLE STUDENTS
+(
+    number int NOT NULL,
+    tId    int NOT NULL,
+    cId    int NOT NULL,
+    PRIMARY KEY (number),
+    FOREIGN KEY (tId) REFERENCES TEAMS (id),
+    FOREIGN KEY (cId) REFERENCES CLASSROOMS (id)
+);
 
 CREATE TABLE ASSIGNMENTS
 (
     id          serial,
-    releaseDate timestamp,
-    cId         int, --class id
-    description varchar(200),
+    releaseDate timestamp    NOT NULL,
+    cId         int          NOT NULL, --class id
+    description varchar(200) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (cId) REFERENCES CLASSROOMS (id)
 );
@@ -108,10 +106,10 @@ CREATE TABLE ASSIGNMENTS
 CREATE TABLE REPOS
 (
     id    serial,
-    url   varchar(50) unique,
-    name  varchar(50),
-    tId   int, --team id
-    assId int, --assignment id
+    url   varchar(50) unique NOT NULL,
+    name  varchar(50)        NOT NULL,
+    tId   int                NOT NULL, --team id
+    assId int                NOT NULL, --assignment id
     PRIMARY KEY (id),
     FOREIGN KEY (tId) REFERENCES TEAMS (id),
     FOREIGN KEY (assId) REFERENCES ASSIGNMENTS (id)
@@ -120,8 +118,8 @@ CREATE TABLE REPOS
 CREATE TABLE DELIVERIES
 (
     id    serial,
-    assId int,       --assignment id
-    date  timestamp, --due date
+    assId int       NOT NULL, --assignment id
+    date  timestamp NOT NULL, --due date
     PRIMARY KEY (id),
     FOREIGN KEY (assId) REFERENCES ASSIGNMENTS (id)
 );
@@ -129,10 +127,10 @@ CREATE TABLE DELIVERIES
 CREATE TABLE TAGS
 (
     id     serial,
-    name   varchar(50),
-    date   timestamp,
-    delId  int, --delivery id
-    teamId int, --team id
+    name   varchar(50) NOT NULL,
+    date   timestamp   NOT NULL,
+    delId  int         NOT NULL, --delivery id
+    teamId int         NOT NULL, --team id
     PRIMARY KEY (id),
     FOREIGN KEY (delId) REFERENCES DELIVERIES (id)
 );
