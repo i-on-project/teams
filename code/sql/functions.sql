e9c5bc1ef00c89b4bb3f54766e6188e117ad416d
47217 - Afonso Machado <A47217@alunos.isel.pt>
2022-05-01 18:00:03 +0000

    Cascade delete triggers done.

--- code/sql/functions.sql
+++ code/sql/functions.sql
@@ -34,3 +34,88 @@ BEGIN
 END
 $check_class_teams_fun$
     LANGUAGE 'plpgsql';
+
+/************************* Cascade Delete Functions *************************/
+
+CREATE OR REPLACE FUNCTION cascade_delete_organizations_fun()
+    RETURNS TRIGGER AS
+$cascade_delete_organizations_fun$
+BEGIN
+    IF (old.deleted = B'0' AND new.deleted = B'1') THEN
+        BEGIN
+            UPDATE classrooms SET deleted = B'1' WHERE orgid=new.id;
+        END;
+    END IF;
+
+    IF (old.state != new.state ) THEN
+        BEGIN
+            UPDATE classrooms SET state = new.state WHERE orgid=new.id;
+        END;
+    END IF;
+END
+$cascade_delete_organizations_fun$
+    LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION cascade_delete_classrooms_fun()
+    RETURNS TRIGGER AS
+$cascade_delete_classrooms_fun$
+BEGIN
+    IF (old.deleted = B'0' AND new.deleted = B'1') THEN
+        BEGIN
+            UPDATE teams SET deleted = B'1' WHERE cid=new.id;
+            UPDATE teachers SET deleted = B'1' WHERE cid=new.id;
+            UPDATE invite_links SET deleted = B'1' WHERE cid=new.id;
+            UPDATE assignments SET deleted = B'1' WHERE cid=new.id;
+        END;
+    END IF;
+
+    IF (old.state != new.state ) THEN
+        BEGIN
+            UPDATE teams SET state = new.state WHERE cid=new.id;
+        END;
+    END IF;
+END
+$cascade_delete_classrooms_fun$
+    LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION cascade_delete_teams_fun()
+    RETURNS TRIGGER AS
+$cascade_delete_teams_fun$
+BEGIN
+    IF (old.deleted = B'0' AND new.deleted = B'1') THEN
+        BEGIN
+            UPDATE students SET deleted = B'1' WHERE tid=new.id;
+            UPDATE notes SET deleted = B'1' WHERE tid=new.id;
+            UPDATE repos SET deleted = B'1' WHERE tid=new.id;
+        END;
+    END IF;
+END
+$cascade_delete_teams_fun$
+    LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION cascade_delete_assignments_fun()
+    RETURNS TRIGGER AS
+$cascade_delete_assignments_fun$
+BEGIN
+    IF (old.deleted = B'0' AND new.deleted = B'1') THEN
+        BEGIN
+            UPDATE deliveries SET deleted = B'1' WHERE assid=new.id;
+        END;
+    END IF;
+END
+$cascade_delete_assignments_fun$
+    LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION cascade_delete_repos_fun()
+    RETURNS TRIGGER AS
+$cascade_delete_repos_fun$
+BEGIN
+    IF (old.deleted = B'0' AND new.deleted = B'1') THEN
+        BEGIN
+            UPDATE tags SET deleted = B'1' WHERE repoid=new.id;
+        END;
+    END IF;
+END
+$cascade_delete_repos_fun$
+    LANGUAGE 'plpgsql';
+
--- code/sql/triggers.sql
+++ code/sql/triggers.sql
@@ -1,4 +1,4 @@
---NOT TESTED
+--Classroom Counters Triggers
 CREATE TRIGGER check_team_members_counter
     AFTER INSERT OR UPDATE
     ON students
@@ -10,3 +10,35 @@ CREATE TRIGGER check_team_counter
     ON teams
     FOR EACH ROW
 EXECUTE PROCEDURE check_class_teams_fun();
+
+/************************* Cascade Delete Triggers *************************/
+
+CREATE TRIGGER cascade_delete_organizations
+    AFTER UPDATE
+    ON organizations
+    FOR EACH ROW
+EXECUTE PROCEDURE cascade_delete_organizations_fun();
+
+CREATE TRIGGER cascade_delete_classrooms
+    AFTER UPDATE
+    ON classrooms
+    FOR EACH ROW
+EXECUTE PROCEDURE cascade_delete_classrooms_fun();
+
+CREATE TRIGGER cascade_delete_teams
+    AFTER UPDATE
+    ON teams
+    FOR EACH ROW
+EXECUTE PROCEDURE cascade_delete_teams_fun();
+
+CREATE TRIGGER cascade_delete_assignments
+    AFTER UPDATE
+    ON assignments
+    FOR EACH ROW
+EXECUTE PROCEDURE cascade_delete_assignments_fun();
+
+CREATE TRIGGER cascade_delete_repos
+    AFTER UPDATE
+    ON repos
+    FOR EACH ROW
+EXECUTE PROCEDURE cascade_delete_repos_fun();
