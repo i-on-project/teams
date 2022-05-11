7a912975b1449df3c7e124c8be81e5e979398165
Martim Francisco <a46912@alunos.isel.pt>
2022-05-11 19:03:10 +0100

    Removed unnecessary standard error handling

--- code/jvm/src/main/kotlin/pt/isel/ion/teams/common/errors/ErrorController.kt
+++ code/jvm/src/main/kotlin/pt/isel/ion/teams/common/errors/ErrorController.kt
@@ -12,14 +12,11 @@ import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.http.converter.HttpMessageNotWritableException
 import org.springframework.validation.BindException
 import org.springframework.web.HttpMediaTypeNotAcceptableException
-import org.springframework.web.HttpMediaTypeNotSupportedException
-import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MissingPathVariableException
 import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.ServletRequestBindingException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
-import org.springframework.web.context.request.async.AsyncRequestTimeoutException
 import org.springframework.web.multipart.support.MissingServletRequestPartException
 import org.springframework.web.servlet.NoHandlerFoundException
 import java.net.URI
