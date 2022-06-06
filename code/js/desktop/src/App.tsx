65bc8af06b707da4304ae3107f1c2b1ed09c71a5
Martim Francisco <a46912@alunos.isel.pt>
2022-06-06 17:45:12 +0100

    2022 docs updated

--- code/js/desktop/src/App.tsx
+++ code/js/desktop/src/App.tsx
@@ -1,27 +1,19 @@
 import * as React from 'react'
 import { Outlet, Link } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+
+declare const electron: {
+  notificationApi: {
+      sendNotification: (notification: { t: string, m: string }) => undefined
+  }
+}
 
 export default function App() {
   return (
     <div>
-      <div>
-        <Navbar bg='dark'>
-          <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-      </div>
       <h1>Hello from react component!</h1>
-      <Button onClick={() => {
+      <button className='ui simple button' onClick={() => {
         electron.notificationApi.sendNotification({ t: 'SOME TITLE', m: 'YEEEEET!' })
-      }}>Notify</Button>
+      }}>Notify</button>
     </div>
   )
 }
--- code/js/desktop/src/index.tsx
+++ code/js/desktop/src/index.tsx
@@ -6,7 +6,7 @@ import {
     HashRouter,
 } from "react-router-dom";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = createRoot(
     document.getElementById('root')
