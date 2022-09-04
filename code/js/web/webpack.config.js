bca1976d7c628482f6d5255dc42786467e6c5aeb
Martim Francisco <a46912@alunos.isel.pt>
2022-09-05 00:47:53 +0100

    Added PORT from env to web app

--- code/js/web/webpack.config.js
+++ code/js/web/webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
     mode: 'development',
     devtool: false,
     devServer: {
-        port: 3000,
+        port: process.env.PORT ? process.env.PORT : 3000,
         historyApiFallback: true,
       },
     resolve: {
