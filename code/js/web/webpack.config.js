9326db253432f1af279f5337bc252abb88261213
Martim Francisco <a46912@alunos.isel.pt>
2022-09-05 00:51:10 +0100

    Disabled host check

--- code/js/web/webpack.config.js
+++ code/js/web/webpack.config.js
@@ -2,6 +2,7 @@ module.exports = {
     mode: 'development',
     devtool: false,
     devServer: {
+        disableHostCheck: true,
         port: process.env.PORT ? process.env.PORT : 3000,
         historyApiFallback: true,
       },
